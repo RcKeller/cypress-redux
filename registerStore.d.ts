c4143e22bca51a49be8ab63ea21cd9c086cad8e7
Joshua Lyman <jlyman@ikonscience.com>
2020-04-21 15:43:22 -0600

    Remove extraneous old attempts in declaration file

--- registerStore.d.ts
+++ registerStore.d.ts
@@ -1,8 +1,1 @@
-// import { Store } from "redux";
-
-// // declare module 'cypress-redux/registerStore' {
-//   function registerStore(store: Store): void;
-//   export = registerStore;
-// // }
-
 declare module 'cypress-redux/registerStore';
